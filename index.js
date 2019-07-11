6bcac65420e4ee35ba34d40f8834c03d2747542b
vastlaan <vastlaan@mail.com>
2019-07-11 18:26:08 +0200

    debugging dependencies have influance to img serving

--- index.js
+++ index.js
@@ -8,91 +8,91 @@ const multer = require('multer')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static('public'))
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'tmp/my-uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, "me.jpg")
-  }
-})
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'tmp/my-uploads')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, "me.jpg")
+//   }
+// })
  
-const upload = multer({ storage: storage }).single("uploadImage")
+// const upload = multer({ storage: storage }).single("uploadImage")
 
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
-app.get('/templates/style.css', (req,res)=>{
-	res.sendFile(path.join(__dirname,'templates','style.css'))
-})
-app.get('/tmp/my-uploads/me.jpg', (req,res)=>{
-	res.sendFile(path.join(__dirname,'tmp/my-uploads','me.jpg'))
-})
+// app.get('/templates/style.css', (req,res)=>{
+// 	res.sendFile(path.join(__dirname,'templates','style.css'))
+// })
+// app.get('/tmp/my-uploads/me.jpg', (req,res)=>{
+// 	res.sendFile(path.join(__dirname,'tmp/my-uploads','me.jpg'))
+// })
 
-app.post('/uploadPhoto',(req,res)=>{
-	upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      // A Multer error occurred when uploading.
-      console.log(err)
-    } else if (err) {
-      // An unknown error occurred when uploading.
-      console.log(err,"2")
-    }
+// app.post('/uploadPhoto',(req,res)=>{
+// 	upload(req, res, function (err) {
+//     if (err instanceof multer.MulterError) {
+//       // A Multer error occurred when uploading.
+//       console.log(err)
+//     } else if (err) {
+//       // An unknown error occurred when uploading.
+//       console.log(err,"2")
+//     }
  
-    // Everything went fine.
-    console.log("done")
-    res.status(200).json("Ok")
-  })
-})
-app.post('/createPdf', (req,res)=>{
+//     // Everything went fine.
+//     console.log("done")
+//     res.status(200).json("Ok")
+//   })
+// })
+// app.post('/createPdf', (req,res)=>{
 	
-	const data=req.body
+// 	const data=req.body
 
-	if(process.env.NODE_ENV==='production'){
-		data.root = 'https://royalcvmaker.herokuapp.com'
-	}else{
-		data.root = 'http://localhost:5000'
-	}
+// 	if(process.env.NODE_ENV==='production'){
+// 		data.root = 'https://royalcvmaker.herokuapp.com'
+// 	}else{
+// 		data.root = 'http://localhost:5000'
+// 	}
 
-	console.log(data) //remove it
+// 	console.log(data) //remove it
 
-	const compile = async (templateName)=>{
-		const filePath =  path.join(process.cwd(),'templates',`${templateName}.html`)
-		const html = await fs.readFile(filePath, 'utf-8')
-		return hbs.compile(html)(data)
+// 	const compile = async (templateName)=>{
+// 		const filePath =  path.join(process.cwd(),'templates',`${templateName}.html`)
+// 		const html = await fs.readFile(filePath, 'utf-8')
+// 		return hbs.compile(html)(data)
 
-	}
-	(async ()=>{
+// 	}
+// 	(async ()=>{
 
-		try{
-			const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
-			const page = await browser.newPage()
-			const content = await compile('index')
-			//await page.setContent(content)
+// 		try{
+// 			const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+// 			const page = await browser.newPage()
+// 			const content = await compile('index')
+// 			//await page.setContent(content)
 
-			await page.goto(`data:text/html;charset=UTF-8,${content}`, {
-				waitUntil: 'networkidle0'
-			})
-			await page.emulateMedia('screen')
-			await page.pdf({
-				path:'mypdf.pdf',
-				format:"A4",
-				printBackground: true
-			})
+// 			await page.goto(`data:text/html;charset=UTF-8,${content}`, {
+// 				waitUntil: 'networkidle0'
+// 			})
+// 			await page.emulateMedia('screen')
+// 			await page.pdf({
+// 				path:'mypdf.pdf',
+// 				format:"A4",
+// 				printBackground: true
+// 			})
 
-			console.log('done')
-			browser.close()
-			res.sendFile(path.join(__dirname,'mypdf.pdf'))
-		}
-		catch(e){
-			console.log(e)
-			res.status(400).json(e)
-		}
+// 			console.log('done')
+// 			browser.close()
+// 			res.sendFile(path.join(__dirname,'mypdf.pdf'))
+// 		}
+// 		catch(e){
+// 			console.log(e)
+// 			res.status(400).json(e)
+// 		}
 		
-	})()
-})
+// 	})()
+// })
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
